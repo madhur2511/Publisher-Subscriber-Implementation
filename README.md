8846992620643fe1d5a6e6e0d53de5d1a6c23ebc
Madhur Kapoor <madhur2511@gmail.com>
2015-10-28 01:15:21 -0700

    Updated python files with apropriate comments where required

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
 # Publisher-Subscriber-Implementation
 This is a simple implementation of Publisher/Subscriber implementation in Python on a single machine and not over the network.
 
-The original Publisher Subscriber Design pattern would be implemented over the network with the publisher and subscribers \
-being remotely located and both using a common messaging messaging queue/broker to communicate/interact, without ever \
-knowing about each other directly. This gives rise to a loosely coulpled architecture for message passing.
+The original Publisher Subscriber Design pattern would be implemented over the network with the publisher and subscribers
+being remotely located and both using a common messaging messaging queue/broker to communicate/interact, without ever
+knowing about each other directly. This gives rise to a loosely-coupled architecture for message passing.
 
-However, this is an example implementation which is all local in a client machine and doesnt involve network programming.\
-The publishers, subscribers and message broker are all classes in the same machine, but this is not how they would be in real setting.\
-Where they would be 3 different components in preferably 3 different servers/clients for reliability.
+However, this is an example implementation which is all local in a client machine and doesnt involve network programming.
+The publishers, subscribers and message broker are all classes in the same machine, but this is not how they would be in real setting. Where they would be 3 different components in preferably 3 different servers/clients for reliability.
 
-So assume that all the 3 components/classes present here are running on 3 different servers/clients and interact over the \
+So lets say that all the 3 components/classes present here are running on 3 different servers/clients and interact over the
 network by knowing each other's internet addresses.
 
-However, since this is not a network-based implementation, and no addresses are involved. Assume that publishers and \
+However, since this is not a network-based implementation, and no addresses are involved. Assume that publishers and
 subscribers know about the broker using a broker instance available with them, instead of an IP address here.
-Similarly, the broker has an instance of subscribers with itself to send appropriate messages to subscribers instead of their\
+Similarly, the broker has an instance of subscribers with itself to send appropriate messages to subscribers instead of their
 IP addresses.
--- Sphere-Pub:Sub-Main.py
+++ Sphere-Pub:Sub-Main.py
@@ -8,42 +8,65 @@ class Broker:
     between publishers and subscribers, without them knowing about each other"""
 
     def __init__(self):
-        """Initialize the Message Queue broker with an empty resource set - message queue and subscriber/topic mapping.
+        """Initializes the Message Queue broker with an empty resource set - message queue and subscriber/topic mapping.
            This will happen only once in time (hopefully) when the message queue is first started and should run continually"""
+
         self.messaging_queue = []
         self.topic_subscription_mapping = defaultdict(lambda:[])
+
 
     def run_loop(self):
         """This is the main code in the broker which always runs until message queue has more messages to pass\
         else, it waits for any more messages. """
+
         for message in self.messaging_queue:
             message, topic = self.process_message(message)
             subscribers = self.get_subscribers_for_topic(topic)
             self.push_message_to_subscribers(subscribers, message)
 
+
     def on_message(self, message):
+        """This is like an event that will be fired on receipt of a new message by the broker, but is implemented\
+         as a method in current context of non-network based implementation. Drops message if message is lacking a topic"""
+
         if len(message.split(";")) >= 2:
             self.messaging_queue.append(message)
             logger.info("Message received by queue :  " + message)
         else:
             pass
 
+
     def process_message(self, message):
+        """Cleans the messages received by the broker from the publishers."""
+
         message_parts = message.split(";")
         return message_parts[0], message_parts[1]
 
+
     def get_subscribers_for_topic(self, topic):
+        """This method in the broker gets the mapping of subscribers to a given topic"""
+
         subscribers = self.topic_subscription_mapping[topic]
         return subscribers
 
+
     def push_message_to_subscribers(self, subscribers, message):
+        """This is like a method implemented using a socket for sending the information/messages to subscribers using ther addresses\
+        but is like a method called on their instance in this case."""
+
         for subscriber in subscribers:
             subscriber.on_message(message)
 
+
     def subscribe_to_topic(self, subscriber, topic):
+        """Populates the mapping of subcribers to topics"""
+
         self.topic_subscription_mapping[topic].append(subscriber)
 
+
     def topic_data(self, subscriber, topic):
+        """Aggregates messages on a given topic, upon request of a subscriber and prepares it for sending"""
+
         messages_for_topic = [self.process_message(message)[0] for message in self.messaging_queue if self.process_message(message)[1] == topic]
         message_aggregator = ""
         for message in messages_for_topic:
@@ -51,33 +74,53 @@ class Broker:
         self.push_message_to_subscribers([subscriber], message_aggregator)
 
 
+
 class Publisher:
     """Publisher that publishes topic wise content into the message broker"""
 
     def __init__(self, name, broker):
+        """Initializes a publisher and provide it the details of broker to send its topic based messages to"""
+
         self.name = name
         self._Broker = broker
 
+
     def publish(self, message, topic):
+        """Method used by publishers to publish a message on a topic to subscribers via message broker"""
+
         _message = message + ";" + topic
         logger.info(self.name + "  , Publishing a message : " + message + " in topic :  " + topic)
         self._Broker.on_message(_message)
+
 
 
 class Subscriber:
     """Subscriber that subscribes to topics with the broker"""
 
     def __init__(self, name, broker):
+        """Initializes the subscriber and provides it the broker details to subscribe to"""
+
         self.name = name
         self._Broker = broker
 
+
     def subscribe(self, topic):
+        """Method used by the subscriber to subscribe to the message queue with a given topic to receive updates on it later
+           This is actually supposed to be a network call to the broker IP, with a request to update this particular subscriber"""
+
         self._Broker.subscribe_to_topic(self, topic)
 
+
     def get_topic_data(self, topic):
+        """Method used by subscriber to request all data/messages on a given topic at once. This is again supposed to be a network call"""
+
         self._Broker.topic_data(self, topic)
 
+
     def on_message(self, message):
+        """On the subscriber node, this is a call-back event fired on receipt of a message on some port but in this implementation,
+        it is like a function call on the given subscriber instance"""
+
         logger.info(self.name + "  , Received a message : " + message)
 
 
